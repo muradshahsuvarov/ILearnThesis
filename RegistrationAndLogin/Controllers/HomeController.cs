8eb17f5882fdd0bc0ff539bbd15b0bfba7a779b3
muradshahsuvarov <42478293+muradshahsuvarov@users.noreply.github.com>
2020-08-29 14:34:10 +0300

    first commit

---
+++ RegistrationAndLogin/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationAndLogin.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult myApplications()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Events(
+[id] int IDENTITY(1,1) NOT NULL,
+[text] nvarchar(250) NULL,
+[start_date] datetime NOT NULL,
+[end_date] datetime NOT NULL,
+PRIMARY KEY(id)
+)
